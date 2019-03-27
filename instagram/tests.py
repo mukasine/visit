7c49a5e8f8b3c9185464c075b4e65e28067ebe67
MBOYIRE <mboyire14@gmail.com>
2019-03-27 08:42:21 +0200

    adding margin-left on new-comment.html

--- instagram/tests.py
+++ instagram/tests.py
@@ -3,13 +3,17 @@ from .models import Profile,Image,Comments
 class ProfileTestClass(TestCase):
     # Set up method
     def setUp(self):
-        self.kt = Profile(name="kayitare",picture="insto/pexels-photo-1464216.jpeg ",bio="<p>sghjjrt</p>")
+        self.kayitare= Profile(name="kayitare",picture="insto/pexels-photo-1464216.jpeg",bio="<p>sghjjrt</p>")
+        
     # Testing instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.kt,Profile))
+        self.assertTrue(isinstance(self.kayitare,Profile))
         # Testing Save Method
     def test_save_method(self):
-        self.kt.save_profile()
+        self.kayitare.save_profile()
         profiles = Profile.objects.all()
         self.assertTrue(len(profiles)>0)
+
+        
+        
         
